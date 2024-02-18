403082ca72c2e7e673f91750eaa111b5f48034fa
Xavier Ogay <xaga.ogay@gmail.com>
2024-02-18 20:35:35 +0100

    Remove : from audio bar

--- slstatus/config.h
+++ slstatus/config.h
@@ -66,7 +66,7 @@ static const struct arg args[] = {
 	//{ datetime, "%s",           "%F %T" },
 	//{ wifi_perc, "W: (%3s%% on ", "wlp0s20f3" }
     	{ netspeed_rx, "%sB/s  ", "wlp0s20f3" },
-	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ run_command, " %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
 	{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
 	{ ram_perc, "[RAM  %s%%]   ", NULL	      },
 	{ battery_perc, "[BAT  %s%%]  ", "BAT0"},
